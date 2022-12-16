10cb8fe6d34bba3d7b4c153f26c6fc1e66464086
Michael Kabongo <michael.kabongo@ST1062.lan>
2022-12-16 16:12:23 -0500

    update

---
+++ index.js
@@ -0,0 +1,10 @@
+let disshowmessage = false;
+function showMessage() {
+    const message = 'I like logs';
+
+    for (let i=0; i < 5; i++){
+        console.log(message);
+    }
+}
+
+showMessage();
